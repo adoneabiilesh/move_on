f0e6ae4d1f204babf29bf293708810f1c24fa0aa
adoneabiilesh <hitimu9@gmail.com>
2025-01-23 11:05:57 +0100

    Fix remaining ESLint errors: unescaped quotes, unused imports, and interface issues

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,11 +1,18 @@
 import Container from "@/components/Container";
 import HomeBanner from "@/components/HomeBanner";
 import ProductGrid from "@/components/ProductGrid";
+import {HeroSection} from "@/components/Hero_section";
+import {FeaturedCategories} from "@/components/Featured";
 
 export default function Home() {
   return (
     <div>
+      <div className="bg-[#1F4F35]">
+      <HeroSection />
+      <FeaturedCategories/>
+      </div>
       <Container className="py-10">
+        
         <HomeBanner />
         <ProductGrid />
       </Container>
